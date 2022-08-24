a4b79fa73ac1361be3ccc0138e4f4d9f6b528ef7
Sushant Temkar <sushant.temkar@Yash.com>
2022-08-24 15:09:55 +0530

    First commit

---
+++ src/test/java/com/springboot/rest/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.rest.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
